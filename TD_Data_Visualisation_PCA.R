150cd4e8b6c9bd2d59979c05241657e92e6b2605
RudyLo <63445186+RudyLo@users.noreply.github.com>
2021-09-13 16:22:31 +0200

    Import des données au format csv

---
+++ TD_Data_Visualisation_PCA.R
@@ -0,0 +1,4 @@
+#Import des données au format csv
+menu <- read.csv("C:/Users/Rudyl/Desktop/ESIEA/Cours_5A/Data_Visualisation/Data_visualisation_tp/menu.csv")
+menu <- as.data.frame(menu)
+summary(menu)
