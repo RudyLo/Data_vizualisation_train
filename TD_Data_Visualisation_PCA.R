9dcdc1324c9b73e76fd6cf4383d8a8d2ce2c31a9
RudyLo <63445186+RudyLo@users.noreply.github.com>
2021-09-13 16:24:31 +0200

    Détermination de la loi normale bivarieé

--- TD_Data_Visualisation_PCA.R
+++ TD_Data_Visualisation_PCA.R
@@ -6,3 +6,7 @@ summary(menu)
 #Intallation du package MVN
 install.packages("MVN")
 library(MVN)
+
+#Détermination de la loi normale bivariée entre deux variables
+couple_to_test <- c("Calories", "Total.Fat")
+result = mvn(menu[couple_to_test], mvnTest = "mardia", univariateTest = "SW", univariatePlot = "histogram", multivariatePlot = "qq", multivariateOutlierMethod = "adj", showOutliers = TRUE, showNewData = TRUE)
