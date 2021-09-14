60952765a05f3e9e8349957dfd2f7ff6639c01ce
RudyLo <63445186+RudyLo@users.noreply.github.com>
2021-09-14 18:18:25 +0200

    Même analyse que sur la précédente, en changeant de jeu de données

--- TD_Data_Visualisation_PCA.R
+++ TD_Data_Visualisation_PCA.R
@@ -58,6 +58,7 @@ plot3d(ellipse3d(cor(cbind(menu.cr_df$Calories, menu.cr_df$Total.Fat,menu.cr_df$
 install.packages("ade4")
 library("ade4")
 
+
 #Utilisation de dudi.pca pour réaliser l'ACP
 list <- c("Calories","Total.Fat","Cholesterol")
 acp <- dudi.pca(menu[, list], center=TRUE, scale=TRUE, scannf = FALSE, nf = 3)
@@ -100,3 +101,51 @@ gcol <- c("red1", "red4","orange")
 s.class(dfxy = acp$li, fac = menu$Category, col = gcol, xax = 1, yax = 2)
 
 scatter(acp)
+
+#Centrer les données avec scale et les réduire
+list <- c("Sodium", "Sugars", "Protein")
+menu.cr <- scale(menu[, list])
+
+#Utilisation de dudi.pca pour réaliser l'ACP sur les 3 autres variables
+list <- c("Sodium","Sugars","Protein")
+acp <- dudi.pca(menu[, list], center=TRUE, scale=TRUE, scannf = FALSE, nf = 3)
+names(acp)
+
+#Mise à l'échelle entre le scale et dudi.pca
+var.n <- function(x) sum((x-mean(x))^2)/length(x)
+scale.n <- function(x) (x - mean(x))/sqrt(var.n(x))
+new_menu <- head(apply(menu.cr, 2, scale.n))
+head(new_menu)
+head(acp$tab)
+
+#Mesure de l'inertie totale
+pve <- 100*acp$eig/sum(acp$eig)
+pve
+cumsum(pve)
+
+#Analyse des variables 
+#Calcul de l'inertie
+
+inertie <- inertia.dudi(acp, col.inertia=TRUE)
+inertie
+
+#Coordonnées des attributs
+round(acp$co,2)
+
+#Cercle des corrélations linéaires
+s.corcircle(acp$co, xax=1, yax=2)
+
+#Représentation des individus sur le premier plan
+
+s.label(acp$li, xax = 1, yax = 2)
+
+#Affichage de l'item
+
+s.label(acp$li, xax = 1, yax = 2, label=as.character(menu$Item), clabel=1.5)
+
+#On ajoute une variable qualitative qui définis les individus par exemples
+gcol <- c("red1", "red4","orange")
+s.class(dfxy = acp$li, fac = menu$Category, col = gcol, xax = 1, yax = 2)
+
+scatter(acp)
+
