f14bbf84a32878ea8c1f43130903b28073bcd935
RudyLo <63445186+RudyLo@users.noreply.github.com>
2021-10-01 12:00:06 +0200

    Affichage des données sous forme de screeplot et affichage des scores de l'ACM

--- TD3/TD3_TCHIASSO_LOMBARD.R
+++ TD3/TD3_TCHIASSO_LOMBARD.R
@@ -29,3 +29,44 @@ summary(poke.x)
 
 #Nous avons 2 variables qualitatives et 1 variable qualitative
 
+
+#1.2 ACM avec ade4
+
+#7. À l’aide de la bibliothèque ade4 et de la bibliothèque adegraphics, appliquez la fonction dudi.acm() au jeu de données poke.x.
+
+res.acm.poke<-dudi.acm(poke.x,scannf=FALSE)
+
+#Nous devons convertir nos données en type factor avant de pouvoir appliquer l'acm sur poke.x :
+poke.x$Type.1 <- as.factor(poke.x$Type.1)  
+poke.x$Generation <- as.factor(poke.x$Generation)  
+poke.x$Legendary <- as.factor(poke.x$Legendary)  
+
+#Nous pouvons maintenant appliquer l'acm
+res.acm.poke<-dudi.acm(poke.x,scannf=FALSE)
+
+#Installation du package devetools et Factoextra pour analyser nos valeurs propres
+if(!require(devtools)) install.packages("devtools")
+devtools::install_github("kassambara/factoextra")
+
+library("factoextra")
+
+fviz_screeplot(res.acm.poke)
+val <- get_eig(res.acm.poke)
+
+# Seuls les axes dont l'inertie est supérieure à l'inertie moyenne sont retenus
+
+mean(val$variance.percent)
+(val$variance.percent) > 4.347826
+
+#On observe qu'il y à plusieurs dimensions qui répondent au critères plus haut, dans notre cas, nous conservons donc les 6 premières.
+
+
+res.x<-val[c(1,2,3,4,5,6),]
+
+score(res.acm.poke,xax=1)
+
+head ( inertia.dudi(res.acm.poke)$tot )
+
+
+
+
